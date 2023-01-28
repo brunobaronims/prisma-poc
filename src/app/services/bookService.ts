c17ecf75a78894ba16cead03951d3f018cf5970f
Bruno Baroni <brunobaronims@gmail.com>
2023-01-28 15:20:47 -0300

    Clean get books response format

--- src/app/services/bookService.ts
+++ src/app/services/bookService.ts
@@ -1,12 +1,17 @@
 import { CreatedBookParams } from "../types";
 import bookRepository from "../../data/repositories/bookRepository";
+import { exclude } from "../../utils/prisma-utils";
 
 async function addBook(params: CreatedBookParams) {
     return bookRepository.addBook(params);
 };
 
 async function getBooksList(userId: number) {
-    return bookRepository.getBooks(userId);
+    const booksList = await bookRepository.getBooks(userId);
+    const formattedList = booksList.map((book) => {
+        return exclude(book, "userId", "id");
+    })
+    return formattedList;
 }
 
 const bookService = {
--- src/data/repositories/bookRepository.ts
+++ src/data/repositories/bookRepository.ts
@@ -1,10 +1,13 @@
 import { Book } from '@prisma/client';
 
 import { prisma } from '../../config/database';
-import { CreatedBookParams, UpdatedBookParams } from '../../app/types/index';
+import {
+    CreatedBookParams,
+    UpdatedBookParams
+} from '../../app/types/index';
 
 async function getBooks(userId: number): Promise<Book[]> {
-    const booksList = await prisma.book.findMany({
+    const booksList: Book[] = await prisma.book.findMany({
         where: {
             userId: userId
         }
@@ -17,7 +20,7 @@ async function addBook(createdBook: CreatedBookParams) {
     const newBook = await prisma.book.create({
         data: createdBook
     })
-    
+
     return newBook;
 };
 
